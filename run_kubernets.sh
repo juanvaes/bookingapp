37363295e28e387876eb3f1b07923bf3d55fe8fd
Juan Valencia <juanvalencia@Juans-MacBook-Pro.local>
2021-11-02 19:21:23 -0500

    Add kubernetes job

---
+++ run_kubernets.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd /home/ubuntu/bookingapp/project
+echo "Initializing conntrack and socat..."
+sudo apt-get install -y conntrack
+sudo apt-get install -y socat
+echo "Initializing cluster..."
+sudo minikube start --driver=none
+sleep 10
+sudo kubectl get nodes
+sudo kubectl create deployment bookingapp-deployment --image=juanvaes/bookingapp:latest
+sudo kubectl get deployments
+sudo kubectl expose deployment/bookingapp-deployment --type="NodePort" --port=80
+sudo kubectl describe services/bookingapp-deployment
+export NODE_PORT=$(sudo kubectl get services/bookingapp-deployment -o go-template='{{(index .spec.ports 0).nodePort}}')
+echo NODE_PORT=$NODE_PORT
+curl $(sudo minikube ip):$NODE_PORT/index
+sudo kubectl port-forward --address 0.0.0.0 deployment/bookingapp-deployment 80:80
