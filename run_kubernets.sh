7fa05a8ceef6931cc0a31c994d743c49776f9cd1
Juan Valencia <juan.valencia@Juan-Valencia-MacBook-Pro.local>
2021-11-05 17:58:59 -0500

    run_kubernetes.sh updates

--- run_kubernets.sh
+++ run_kubernets.sh
@@ -11,7 +11,8 @@ sudo kubectl create deployment bookingapp-deployment --image=juanvaes/bookingapp
 sudo kubectl get deployments
 sudo kubectl expose deployment/bookingapp-deployment --type="NodePort" --port=80
 sudo kubectl describe services/bookingapp-deployment
-sudo kubectl wait --for=condition=Ready pod -l app=bookingapp-deployment --timeout=30s
+export POD_NAME=$(sudo kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+sudo kubectl wait --for=condition=Ready pod/$POD_NAME --timeout=-1s
 export NODE_PORT=$(sudo kubectl get services/bookingapp-deployment -o go-template='{{(index .spec.ports 0).nodePort}}')
 echo NODE_PORT=$NODE_PORT
 curl $(sudo minikube ip):$NODE_PORT/index
