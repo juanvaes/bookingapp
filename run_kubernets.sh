bb2ebb27cd5ecae0750ecf754d8f82e34c4c51cd
Juan Valencia <juan.valencia@Juan-Valencia-MacBook-Pro.local>
2021-11-05 18:44:34 -0500

    Updates

--- run_kubernets.sh
+++ run_kubernets.sh
@@ -6,14 +6,7 @@ sudo apt-get install -y conntrack
 sudo apt-get install -y socat
 echo "Initializing cluster..."
 sudo minikube start --driver=none
+sudo kubectl run bookingapp --image=juanvaes/bookingapp:latest --port=80 --labels app=bookingapp
+sudo kubectl wait pod/bookingapp --for=condition=Ready --timeout=-1s
 sudo kubectl get nodes
-sudo kubectl create deployment bookingapp-deployment --image=juanvaes/bookingapp:latest
-sudo kubectl get deployments
-sudo kubectl expose deployment/bookingapp-deployment --type="NodePort" --port=80
-sudo kubectl describe services/bookingapp-deployment
-export POD_NAME=$(sudo kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
-sudo kubectl wait --for=condition=Ready pod/$POD_NAME --timeout=-1s
-export NODE_PORT=$(sudo kubectl get services/bookingapp-deployment -o go-template='{{(index .spec.ports 0).nodePort}}')
-echo NODE_PORT=$NODE_PORT
-curl $(sudo minikube ip):$NODE_PORT/index
-sudo kubectl port-forward --address 0.0.0.0 deployment/bookingapp-deployment 80:80
+sudo kubectl port-forward --address 0.0.0.0 pod/bookingapp 80:80
