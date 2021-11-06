05e46b4acc25e25c9c4e631e49c9e92be878ad92
Juan Valencia <juan.valencia@Juan-Valencia-MacBook-Pro.local>
2021-11-05 19:16:07 -0500

    Wait resources

--- run_kubernets.sh
+++ run_kubernets.sh
@@ -6,6 +6,9 @@ sudo apt-get install -y conntrack
 sudo apt-get install -y socat
 echo "Initializing cluster..."
 sudo minikube start --driver=none
+export NODE_NAME=$(sudo kubectl get nodes --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo $NODE_NAME
+sudo kubectl wait node/$NODE_NAME --for=condition=Ready --timeout=-1s
 sudo kubectl run bookingapp --image=juanvaes/bookingapp:latest --port=80 --labels app=bookingapp
 sudo kubectl wait pod/bookingapp --for=condition=Ready --timeout=-1s
 sudo kubectl get nodes
