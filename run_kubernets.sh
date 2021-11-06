52236e2e874e4e1310ba3a3bd7c7f0c5eed4dc5b
Juan Valencia <juan.valencia@Juan-Valencia-MacBook-Pro.local>
2021-11-06 10:55:14 -0500

    Wait resources

--- run_kubernets.sh
+++ run_kubernets.sh
@@ -8,8 +8,10 @@ echo "Initializing cluster..."
 sudo minikube start --driver=none
 export NODE_NAME=$(sudo kubectl get nodes --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 echo $NODE_NAME
+echo "Waiting for node to be ready..."
 sudo kubectl wait node/$NODE_NAME --for=condition=Ready --timeout=-1s
 sudo kubectl run bookingapp --image=juanvaes/bookingapp:latest --port=80 --labels app=bookingapp
+echo "Waiting for pod to be ready..."
 sudo kubectl wait pod/bookingapp --for=condition=Ready --timeout=-1s
 sudo kubectl get nodes
 sudo kubectl port-forward --address 0.0.0.0 pod/bookingapp 80:80
