5d72795f5d6dac9ba02dfd0f4729d809bcfb5405
Juan Valencia <juanvalencia@Juans-MacBook-Pro.local>
2021-11-09 18:34:44 -0500

    updates

--- run_kubernets.sh
+++ run_kubernets.sh
@@ -3,14 +3,17 @@ echo "----------------------------"
 echo "----Deploying Kubernetes----"
 echo "----------------------------"
 
-echo "Listing service accounts..."
-kubectl get serviceAccounts
-echo "Running POD..."
-kubectl run bookingapp-pod --image=juanvaes/bookingapp:latest --port=80 --labels app=bookingapp
-echo "Listing pods..."
-kubectl get pods
-echo "Waiting for pod to be ready..."
-kubectl wait pod/bookingapp-pod --for=condition=Ready --timeout=-1s
-kubectl get nodes
+#echo "Listing service accounts..."
+#kubectl get serviceAccounts
+#echo "Running POD..."
+#kubectl run bookingapp-pod --image=juanvaes/bookingapp:latest --port=80 --labels app=bookingapp
+#echo "Listing pods..."
+#kubectl get pods
+#echo "Waiting for pod to be ready..."
+#kubectl wait pod/bookingapp-pod --for=condition=Ready --timeout=-1s
+sudo kubectl apply -f app-deployment.yml
+sudo kubectl get nodes
+export POD_NAME=$(sudo kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+sudo kubectl wait --for=condition=Ready pod/$POD_NAME --timeout=-1s
 echo "Forwarding port..."
-kubectl port-forward --address 0.0.0.0 pod/bookingapp-pod 80:80
+sudo kubectl port-forward --address 0.0.0.0 pod/bookingapp-pod 80:80
