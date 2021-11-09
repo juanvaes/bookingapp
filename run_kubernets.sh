a823977a63c429699772454a6986dad6fa84b6b4
Juan Valencia <juanvalencia@Juans-MacBook-Pro.local>
2021-11-09 08:36:15 -0500

    updates

--- run_kubernets.sh
+++ run_kubernets.sh
@@ -10,8 +10,9 @@ export NODE_NAME=$(sudo kubectl get nodes --template '{{range .items}}{{.metadat
 echo $NODE_NAME
 echo "Waiting for node to be ready..."
 sudo kubectl wait node/$NODE_NAME --for=condition=Ready --timeout=-1s
+sudo kubectl get serviceAccounts
 sudo kubectl run bookingapp-pod --image=juanvaes/bookingapp:latest --port=80 --labels app=bookingapp
 echo "Waiting for pod to be ready..."
 sudo kubectl wait pod/bookingapp-pod --for=condition=Ready --timeout=-1s
 sudo kubectl get nodes
-sudo kubectl port-forward --address 0.0.0.0 pod/bookingapp 80:80
+sudo kubectl port-forward --address 0.0.0.0 pod/bookingapp-pod 80:80
