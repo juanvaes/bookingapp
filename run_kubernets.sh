876bc56f0e605f1539229d12b7720c5b03e8d56a
Juan Valencia <juanvalencia@Juans-MacBook-Pro.local>
2021-11-09 09:35:44 -0500

    split and conquer

--- run_kubernets.sh
+++ run_kubernets.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
+echo "----------------------------"
+echo "----Deploying Kubernetes----"
+echo "----------------------------"
 
-cd /home/ubuntu/bookingapp/project
-echo "Initializing conntrack and socat..."
-sudo apt-get install -y conntrack
-sudo apt-get install -y socat
-echo "Initializing cluster..."
-sudo minikube start --driver=none
-export NODE_NAME=$(sudo kubectl get nodes --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
-echo $NODE_NAME
-echo "Waiting for node to be ready..."
-sudo kubectl wait node/$NODE_NAME --for=condition=Ready --timeout=-1s
+echo "Listing service accounts..."
 sudo kubectl get serviceAccounts
+echo "Running POD..."
 sudo kubectl run bookingapp-pod --image=juanvaes/bookingapp:latest --port=80 --labels app=bookingapp
-echo "Waiting for pod to be ready..."
+echo "Listing pods..."
 sudo kubectl get pods
+echo "Waiting for pod to be ready..."
 sudo kubectl wait pod/bookingapp-pod --for=condition=Ready --timeout=-1s
 sudo kubectl get nodes
+echo "Forwarding port..."
 sudo kubectl port-forward --address 0.0.0.0 pod/bookingapp-pod 80:80
