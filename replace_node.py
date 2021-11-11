bfc76089df937cb921d02ef20b4a0f657123a98a
Juan Valencia <juanvalencia@Juans-MacBook-Pro.local>
2021-11-10 21:16:27 -0500

    find node port with subprocess

--- replace_node.py
+++ replace_node.py
@@ -1,13 +1,17 @@
-import os
-import pathlib
+import subprocess
 
 def find_node_port():
-    return os.system(
-       "sudo kubectl get -o jsonpath='{.spec.ports[0].nodePort}' services bookingapp"
-    )
+    process = subprocess.Popen([
+        "sudo", "kubectl", "get", "-o", "jsonpath='{.spec.ports[0].nodePort}'", "services", "bookingapp"],
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE
+        )
+    stdout, stderr = process.communicate()
+    return stdout.decode("utf-8").replace("'","")
+    
 
 def main():
-    project_path = str(pathlib.Path(".").absolute())
+    project_path = "/home/ubuntu/bookingapp/project/nginx.conf"
     nginx_path_file = project_path + "/nginx.conf"
     with open(nginx_path_file, "r") as nginx:
         nginx_commands = nginx.read()
